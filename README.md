7a47f5344a51e736d22cb53e9c2cbd1b75a5d56e
Joshua Whitley <josh.whitley@autoware.org>
2020-05-11 13:10:55 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+Gitlab Graph Generator
+======================
+
+A set of Python scripts for generating graphs from the Gitlab API.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GitlabGraphGenerator',
+    version='0.1',
+    packages=['gitlabgraphgenerator',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
